ccc18394989a0fd00df8c63a65c56962874054c2
Vadim Zaytsev <z.v.m.365@gmail.com>
2024-02-10 15:00:14 +0300

    lesson-02 task-06

---
+++ lesson-02/06.js
@@ -0,0 +1,29 @@
+let passportWithAddress = {
+    name: 'Petr',
+    surname: 'Petrov',
+    address: {
+        country: 'USA',
+        city: 'LA',
+    },
+};
+
+function deepCopy(obj) {
+    if (typeof obj !== 'object' || obj === null) {
+        return obj;
+    }
+
+    const copy = Array.isArray(obj) ? [] : {};
+
+    for (const key in obj) {
+        copy[key] = deepCopy(obj[key]);
+    }
+
+    return copy;
+}
+
+const passportWithAddressCopy = deepCopy(passportWithAddress);
+
+passportWithAddressCopy.address.city = 'Bobryisk';
+
+console.log(passportWithAddress);
+console.log(passportWithAddressCopy);
