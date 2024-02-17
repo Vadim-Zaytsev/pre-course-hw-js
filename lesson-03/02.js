c782d09eefa3b8183ce3ecbf41d1dd3d24063111
Vadim Zaytsev <z.v.m.365@gmail.com>
2024-02-17 11:51:04 +0300

    Lesson-3 task-2

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,9 +1,7 @@
 // eslint-disable-next-line
 const words = ['яблоко', 'банан', 'апельсин', 'манго', 'киви'];
 
-const index = words.indexOf('апельсин');
-
-if (index !== -1) {
+if (words.indexOf('апельсин') !== -1) {
     console.log('Ура! нашел');
 } else {
     console.log('Придется поискать в другом магазине…');
